29a769713947f78f1ad6c107fc25ce8def5f5cbe
Kami Ruebush <kami691@revature.net>
2022-04-28 19:31:03 -0500

    Robot Grid

---
+++ src/main/java/com/onboard/robot_grid/App.java
@@ -0,0 +1,13 @@
+package com.onboard.robot_grid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/onboard/robot_grid_main/Robot_Grid.java
@@ -0,0 +1,5 @@
+package com.onboard.robot_grid_main;
+
+public class Robot_Grid {
+
+}
